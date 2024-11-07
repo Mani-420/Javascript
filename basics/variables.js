fd6e8ffe6e7b17fcf23356fef9945ea5e0479e98
Mani-420 <tahirabdulrehman46@gmail.com>
2024-11-07 23:17:56 +0500

    Variables in Javascript

---
+++ basics/variables.js
@@ -0,0 +1,25 @@
+const accountId = 7                     // when const is used it cannot be changed
+let accountEmail = "mithu@google.com"
+var accountPassword = "12345"
+let accountCity = "Lahore"
+let accountState = true;
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Islamabad"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+// Wrong method 
+// console.table(accountId, accountEmail, accountPassword, accountCity, accountState)
+
+// Right Method 
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
