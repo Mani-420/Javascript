f4fca0f99702ecea3f61b0933b1b064da8188e74
Mani-420 <tahirabdulrehman46@gmail.com>
2024-11-10 18:14:59 +0500

    Scope

---
+++ functions/scope.js
@@ -0,0 +1,55 @@
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "Mani"
+
+    function two(){
+        const website = "mani.com"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+}
+
+// one()
+
+if (true) {
+    const username = "mani123"
+    if (username === "mani123") {
+        const website = "google.com"
+        console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// // ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+const addTwo = function(num){
+    return num + 2
+}
+
+addTwo(5)
