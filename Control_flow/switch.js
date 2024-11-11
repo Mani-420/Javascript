652246f772605819f9a194194d65f9313bf3c6c3
Mani-420 <tahirabdulrehman46@gmail.com>
2024-11-11 23:17:11 +0500

    Switch Statement

---
+++ Control_flow/switch.js
@@ -0,0 +1,37 @@
+// Basic Syntax ----------------------------------------
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// Example ----------------------------------------
+
+const month = "march"
+
+switch (month) {
+    
+    case "jan":
+        console.log("January");
+        break;
+
+    case "feb":
+        console.log("feb");
+        break;
+
+    case "march":
+        console.log("march");
+        break;
+
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
