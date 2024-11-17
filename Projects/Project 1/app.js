38aebf939c42b8fb63d29ddc470ecbf25338f999
Mani-420 <tahirabdulrehman46@gmail.com>
2024-11-18 00:12:20 +0500

    Background Color Changer Project

---
+++ Projects/Project 1/app.js
@@ -0,0 +1,29 @@
+// console.log("Mani Tahir")
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+buttons.forEach(function (button) {
+  console.log(button);
+  button.addEventListener('click', function (event) {
+    // console.log(event);
+    // console.log(event.target);
+    if (event.target.id === 'grey') {
+      body.style.backgroundColor = event.target.id;
+      body.style.color = 'white'
+    }
+    if (event.target.id === 'white') {
+        body.style.backgroundColor = event.target.id;
+        body.style.color = 'black'
+    }
+    if (event.target.id === 'blue') {
+        body.style.backgroundColor = event.target.id;
+        body.style.color = 'white'
+    }
+    if (event.target.id === 'yellow') {
+        body.style.backgroundColor = event.target.id;
+        body.style.color = 'black'
+    }
+    
+  });
+});
+
