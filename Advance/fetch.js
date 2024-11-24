48e4df8f9d942eda330344569194fda58df8eb9c
Mani-420 <tahirabdulrehman46@gmail.com>
2024-11-24 23:23:32 +0500

    Fetch

---
+++ Advance/fetch.js
@@ -0,0 +1,10 @@
+fetch('https://api.github.com/users/Mani-420')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error))
+
+// promise.all
