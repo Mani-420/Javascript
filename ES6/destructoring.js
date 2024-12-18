87a99b8f64109d40b2ed8cddb9cc3e50f67a9da1
Mani-420 <tahirabdulrehman46@gmail.com>
2024-12-18 22:54:49 +0500

    Destructuring in JS

---
+++ ES6/destructoring.js
@@ -0,0 +1,11 @@
+const address = {
+    street: 'Street 4',
+    city: 'Lahore',
+    country: 'Pakistan',
+};
+
+// const street = address.street;
+// const city = address.city;
+// const country = address.country;
+
+const {street, city, country} = address
